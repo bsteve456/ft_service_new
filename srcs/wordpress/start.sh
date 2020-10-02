f1fb5eaec12da68a3006454bae96ed4e4d3d5f21
bsteve456 <bstevealan@gmail.com>
2020-10-02 20:50:21 +0200

    modif config

--- srcs/wordpress/start.sh
+++ srcs/wordpress/start.sh
@@ -12,11 +12,13 @@ if [ $? == 1 ]; then
     mysql -h mysql -u root -ppassword -e 'CREATE DATABASE IF NOT EXISTS wordpress';
 
     while [ $? == 1 ]; do
+	    echo "ok";
 	 mysql -h mysql -u root -ppassword -e 'CREATE DATABASE IF NOT EXISTS wordpress';
      done
      echo "good";
      mysql -h mysql -u root -ppassword wordpress < /wordpress.sql;
      while [ $? == 1 ]; do
+	     echo "here";
 	 mysql -h mysql -u root -ppassword wordpress < /wordpress.sql;
      done
 fi
